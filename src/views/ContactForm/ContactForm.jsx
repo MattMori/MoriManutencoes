5e993540925ede14b2843ee50e4cf7d914baf226
mattmori16 <mateus5310@live.com>
2026-01-28 19:34:26 -0300

    atualizado o formulário de contato e estilos da página inicial

--- src/views/ContactForm/ContactForm.jsx
+++ src/views/ContactForm/ContactForm.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react';
-import axios from 'axios';
-import InputMask from 'react-input-mask';
-import styles from './ContactForm.module.scss';
-import Navbar from '../Navbar/Navbar';
-import Footer from '../Footer/Footer';
-import ButtonA from '../../Components/ButtonA';
-import Swal from 'sweetalert2';
+import { useState } from "react";
+import axios from "axios";
+import InputMask from "react-input-mask";
+import styles from "./ContactForm.module.scss";
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
+import ButtonA from "../../Components/ButtonA";
+import Swal from "sweetalert2";
 
 const ContactForm = () => {
-  const [name, setName] = useState('');
-  const [phone, setPhone] = useState('');
-  const [email, setEmail] = useState('');
-  const [description, setDescription] = useState('');
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [description, setDescription] = useState("");
 
   const toast = Swal.mixin({
     toast: true,
@@ -22,16 +22,15 @@ const ContactForm = () => {
     didOpen: (toast) => {
       toast.onmouseenter = Swal.stopTimer;
       toast.onmouseleave = Swal.resumeTimer;
-    }
+    },
   });
-  
+
   const handleNameChange = (e) => {
     setName(e.target.value);
   };
 
   const handlePhoneChange = (e) => {
-    // Remova caracteres não numéricos do número de telefone
-    const formattedPhone = e.target.value.replace(/\D/g, '');
+    const formattedPhone = e.target.value.replace(/\D/g, "");
     setPhone(formattedPhone);
   };
 
@@ -47,27 +46,29 @@ const ContactForm = () => {
     e.preventDefault();
 
     try {
-      const response = await axios.post('https://api.sheetmonkey.io/form/wfB1ETpoMFNJUwbx8qEJS', {
-        name,
-        phone,
-        email,
-        description,
-      });
+      const response = await axios.post(
+        "https://api.sheetmonkey.io/form/wfB1ETpoMFNJUwbx8qEJS",
+        {
+          name,
+          phone,
+          email,
+          description,
+        },
+      );
       toast.fire({
         icon: "success",
-        title: "Solicitação enviada com sucesso!"
+        title: "Solicitação enviada com sucesso!",
       });
 
-      setName('');
-      setPhone('');
-      setEmail('');
-      setDescription('');
-      
+      setName("");
+      setPhone("");
+      setEmail("");
+      setDescription("");
     } catch (error) {
-      console.error('Erro ao enviar formulário:', error);
+      console.error("Erro ao enviar formulário:", error);
       toast.fire({
         icon: "error",
-        title: "verifique os dados e tente novamente!"
+        title: "verifique os dados e tente novamente!",
       });
     }
   };
@@ -116,10 +117,9 @@ const ContactForm = () => {
               value={description}
               required
               onChange={handleDescriptionChange}
-              
             />
           </div>
-          <ButtonA type="submit" text='Enviar' />
+          <ButtonA type="submit" text="Enviar" />
         </form>
       </div>
       <Footer />
--- src/views/Home/Presetation.jsx
+++ src/views/Home/Presetation.jsx
@@ -10,11 +10,11 @@ const Presentation = () => {
         <h1> M & R manutenção e instalação elétrica </h1>{" "}
         <p>
           Olá! Sou o Milton Mori. <br />
-          Há 25 anos, ajudo famílias e empresas a renovarem seus espaços com
-          segurança e qualidade. <br />
+          Há 25 anos, ajudo famílias e empresas a renovarem seus espaços<br /> com
+          segurança e qualidade.  <br />
           Precisa de um serviço bem feito, sem dor de cabeça? <br /> Eu garanto
           um trabalho impecável, seguro e, acima de tudo, limpo. <br /> Do
-          acabamento da pintura à instalação elétrica, cuido da sua casa como se
+          acabamento da pintura à instalação elétrica, <br />cuido da sua casa como se
           fosse a minha.
         </p>{" "}
         <ButtonA link={"/contactForm"} text="Entre em contato" />
