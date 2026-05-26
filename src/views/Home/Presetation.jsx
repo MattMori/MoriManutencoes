4e2dbd98fc98bb6fec2f688b22e0d97d79ad47fd
MattMori <mateus5310@live.com>
2026-05-26 13:51:22 -0300

    ADIÇÃO DE sitemap.xml e robots.txt

--- src/views/Home/Presetation.jsx
+++ src/views/Home/Presetation.jsx
@@ -7,21 +7,25 @@ const Presentation = () => {
   return (
     <div className={styles.wrapper}>
       <Navbar />
-      
+
       <main className={styles.mainContent}>
         <section className={styles.heroCard}>
-          <h1>MoriService - Instalações Elétricas e pintura</h1>
+          <h1>
+            Garantindo o melhor <br /> <span>serviço para </span>
+            sua casa ou empresa.
+          </h1>
 
           <p className={styles.lead}>
-            Sou Milton Mori. Há 25 anos, transformo residências e empresas com 
-            serviços técnicos baseados em segurança, normas rígidas e execução impecável.
+            Sou Milton Mori. Há 25 anos, transformo residências e empresas com
+            serviços técnicos baseados em segurança, normas rígidas e execução
+            impecável.
           </p>
 
           <p className={styles.description}>
-            O segredo de uma obra sem estresse está no respeito ao seu espaço. 
-            Entrego projetos elétricos complexos e acabamentos de pintura com cronograma 
-            à risca e um diferencial inegociável: organização absoluta e ambiente limpo 
-            do início ao fim.
+            O segredo de uma obra sem estresse está no respeito ao seu espaço.
+            Entrego projetos elétricos complexos e acabamentos de pintura com
+            cronograma à risca e um diferencial inegociável: organização
+            absoluta e ambiente limpo do início ao fim.
           </p>
 
           <div className={styles.ctaWrapper}>
@@ -29,7 +33,7 @@ const Presentation = () => {
           </div>
         </section>
       </main>
-      
+
       <Footer />
     </div>
   );
