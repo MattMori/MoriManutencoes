190c87b4d0e4c1e87b6095cb8973449f5d498f26
MattMori <mateus5310@live.com>
2026-06-07 14:17:31 -0300

    correção no GOOGLE_ADS_SEND_TO

--- src/views/ContactForm/ContactForm.jsx
+++ src/views/ContactForm/ContactForm.jsx
@@ -13,8 +13,7 @@ function ContactForm() {
   });
 
   const WHATSAPP_NUMBER = "5519988896428";
-  const GOOGLE_ADS_SEND_TO = "AW-18191638120/yTQqCNGhxLocEOi8ueJD";
-
+  const GOOGLE_ADS_SEND_TO = "AW-18191638120/uDumCMqdhLQcEOi8ueJD";
   const handleChange = (event) => {
     const { name, value } = event.target;
 
